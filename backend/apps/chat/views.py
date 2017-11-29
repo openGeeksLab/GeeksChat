7d4d3b519ea714b805e04e8d6743944f139d4b1d
AlexeyR <alexei_r@opengeekslab.com>
2017-11-29 09:09:38 +0200

    fb login

--- backend/apps/chat/views.py
+++ backend/apps/chat/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from rest_auth.registration.views import SocialLoginView
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+
+class FacebookLogin(SocialLoginView):
+    authentication_classes = (TokenAuthentication,)
+    adapter_class = FacebookOAuth2Adapter
--- backend/geeksChat/urls.py
+++ backend/geeksChat/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from graphene_django.views import GraphQLView
+
+from apps.chat.views import FacebookLogin
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^graphql', GraphQLView.as_view(graphiql=True)),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
 ]
